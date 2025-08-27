81c8c64e6434de93d37ba6ee6e4695d6848d748a
EdilsonRogerioCuambe <edilson@aluno.unilab.edu.br>
2025-08-27 00:51:13 -0300

    refactor: simplify app-sidebar component

---
+++ src/components/sidebar/app-sidebar.tsx
@@ -0,0 +1,58 @@
+"use client";
+
+import * as React from "react";
+
+import { navData } from "@/components/sidebar/nav-data";
+import { NavDocuments } from "@/components/sidebar/nav-documents";
+import { NavMain } from "@/components/sidebar/nav-main";
+import { NavSecondary } from "@/components/sidebar/nav-secondary";
+import { NavUser } from "@/components/sidebar/nav-user";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+import Image from "next/image";
+import Link from "next/link";
+
+const data = navData;
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar collapsible="offcanvas" {...props}>
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton
+              asChild
+              className="data-[slot=sidebar-menu-button]:!p-1.5"
+            >
+              <Link href="/dashboard">
+                <Image
+                  src="/Kudondza.svg"
+                  alt="Kudondza Logo"
+                  width={32}
+                  height={32}
+                  className="rounded"
+                />
+                <span className="text-base font-semibold">Kudondza</span>
+              </Link>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
+      <SidebarContent>
+        <NavMain items={data.navMain} />
+        <NavDocuments items={data.documents} />
+        <NavSecondary items={data.navSecondary} className="mt-auto" />
+      </SidebarContent>
+      <SidebarFooter>
+        <NavUser />
+      </SidebarFooter>
+    </Sidebar>
+  );
+}
