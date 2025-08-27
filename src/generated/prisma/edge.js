4237541fa37c1f38dcc77cf6e982f3a4183d5ef2
EdilsonRogerioCuambe <edilson@aluno.unilab.edu.br>
2025-08-27 09:26:33 -0300

    chore(prisma): update generated prisma client files

--- src/generated/prisma/edge.js
+++ src/generated/prisma/edge.js
@@ -1171,7 +1171,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": "../../../.env",
+    "rootEnvPath": null,
     "schemaEnvPath": "../../../.env"
   },
   "relativePath": "../../../prisma",
--- src/generated/prisma/index.js
+++ src/generated/prisma/index.js
@@ -1172,7 +1172,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": "../../../.env",
+    "rootEnvPath": null,
     "schemaEnvPath": "../../../.env"
   },
   "relativePath": "../../../prisma",
