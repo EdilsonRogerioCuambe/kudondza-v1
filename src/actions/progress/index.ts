fa146d8441216b8214acb05aee241da1b7bad201
EdilsonRogerioCuambe <edilson@aluno.unilab.edu.br>
2025-08-27 23:40:11 -0300

    feat(progress): create index file to export all progress actions

---
+++ src/actions/progress/index.ts
@@ -0,0 +1,9 @@
+export { getUserAchievements } from "./get-achievements";
+export { getCommunityRanking } from "./get-community-ranking";
+export {
+  createUserGoal,
+  deleteUserGoal,
+  getUserGoals,
+  updateUserGoal,
+} from "./get-user-goals";
+export { getUserProgress } from "./get-user-progress";
