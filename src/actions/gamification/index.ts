7530b403d118e3be777db226a626c791f9af0670
EdilsonRogerioCuambe <edilson@aluno.unilab.edu.br>
2025-08-28 00:07:37 -0300

    feat(gamification): add index file to export all gamification actions

---
+++ src/actions/gamification/index.ts
@@ -0,0 +1,6 @@
+export { getAchievementsWithProgress } from "./get-achievements";
+export { getBadgesByCategory } from "./get-badges";
+export { getDailyChallenges } from "./get-daily-challenges";
+export { getLeaderboard } from "./get-leaderboard";
+export { getLevelRewards } from "./get-level-rewards";
+export { getUserGamification } from "./get-user-gamification";
