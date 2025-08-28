22e9982d358b7ca6184dcafa8417c128fb1621e2
EdilsonRogerioCuambe <edilson@aluno.unilab.edu.br>
2025-08-28 00:21:31 -0300

    chore(prisma): update generated client files

--- src/generated/prisma/edge.js
+++ src/generated/prisma/edge.js
@@ -1284,6 +1284,7 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
+  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
--- src/generated/prisma/index.js
+++ src/generated/prisma/index.js
@@ -1285,6 +1285,7 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
+  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
