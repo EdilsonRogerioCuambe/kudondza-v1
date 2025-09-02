649c9f0cf83710f646adfd50c664fa0209d9c17a
EdilsonRogerioCuambe <edilson@aluno.unilab.edu.br>
2025-09-02 00:29:26 -0300

    feat(notifications): add index file to export all notification actions

---
+++ src/actions/notifications/index.ts
@@ -0,0 +1,8 @@
+export { createNotification } from "./create-notification";
+export { deleteNotification } from "./delete-notification";
+export { getNotification } from "./get-notification";
+export { getNotifications } from "./get-notifications";
+export { getUnreadNotificationsCount } from "./get-unread-notifications-count";
+export { markAllNotificationsAsRead } from "./mark-all-notifications-as-read";
+export { markNotificationAsRead } from "./mark-notification-as-read";
+export { updateNotification } from "./update-notification";
